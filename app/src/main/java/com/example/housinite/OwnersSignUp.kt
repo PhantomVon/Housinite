dd711e630db6ad34826fb11eab748cd81c676b8c
PhantomVon <p9691402@gmail.com>
2023-05-13 12:49:14 +0300

    first commit

---
+++ app/src/main/java/com/example/housinite/OwnersSignUp.kt
@@ -0,0 +1,11 @@
+package com.example.housinite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OwnersSignUp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_owners_sign_up)
+    }
+}
